dfe0ff206baf767ba1619751cc0e22c80ddb437d
Philipp Middendorf <philipp.middendorf@desy.de>
2022-06-18 14:16:01 +0200

    ✨💄 Use new table widget which looks nicer

--- app/Myocardio/AppState.hs
+++ app/Myocardio/AppState.hs
@@ -6,12 +6,11 @@ import Brick.Widgets.Edit (Editor)
 import Data.Text (Text)
 import Data.Time.Clock (UTCTime)
 import Lens.Micro.Platform (makeLenses)
-import Myocardio.AppListType (AppListType)
 import Myocardio.Data (Data)
 import Myocardio.ResourceName (ResourceName)
 
 data AppState = AppState
-  { _stateList :: AppListType,
+  { _stateTableCursor :: Int,
     _stateEditor :: Editor Text ResourceName,
     _stateData :: Data,
     _stateNow :: UTCTime,
