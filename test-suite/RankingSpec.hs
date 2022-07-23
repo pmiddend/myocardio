711a6a4fbd63140b2d173f7fb72e3e85cdc6c4f3
Philipp Middendorf <philipp.middendorf@desy.de>
2022-07-23 11:40:35 +0200

    ✨ Move to new algorithm

--- test-suite/RankingSpec.hs
+++ test-suite/RankingSpec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module RankingSpec
   ( spec,
@@ -8,43 +9,29 @@ where
 
 import Control.Applicative ((<*>))
 import Data.Bool
-  ( Bool (True, False),
+  ( Bool (True),
     (&&),
   )
-import Data.Eq ((==))
 import Data.Foldable
   ( maximum,
     minimum,
   )
 import Data.Function (($))
 import Data.Functor ((<$>))
-import Data.Int (Int)
-import Data.List
-  ( length,
-    sort,
-  )
-import qualified Data.List.NonEmpty as NE
 import Data.Maybe (Maybe (Just, Nothing))
 import Data.Ord
-  ( Ord,
-    max,
-    min,
-    (<=),
+  ( (<=),
     (>=),
   )
-import Data.String (String)
 import Data.Time.Calendar (fromGregorian)
 import Data.Time.Clock
   ( UTCTime (UTCTime),
     addUTCTime,
   )
 import Myocardio.Exercise (Exercise (Exercise), name, muscles, tagged, last, category, reps)
-import Myocardio.Muscle (Muscle (GluteusMaximus, Hamstrings, Pecs))
+import Myocardio.Muscle (Muscle (GluteusMaximus, Pecs))
 import Myocardio.Ranking
-  ( RankedExercise (RankedExercise),
-    exerciseComplement,
-    generateComplements,
-    rankTime,
+  ( rankTime,
     reorderExercises,
   )
 import Test.Hspec
@@ -56,13 +43,7 @@ import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
 import Test.QuickCheck
   ( Arbitrary (arbitrary),
-    Fun,
-    applyFun2,
     arbitraryBoundedEnum,
-  )
-import Test.QuickCheck.Arbitrary
-  ( Arbitrary,
-    arbitrary,
   )
 import Test.QuickCheck.Instances.Semigroup
   (
@@ -70,34 +51,12 @@ import Test.QuickCheck.Instances.Semigroup
 import Test.QuickCheck.Instances.Text ()
 import Test.QuickCheck.Instances.Time ()
 import Prelude
-  ( Char,
-    realToFrac,
-    (-),
+  ( realToFrac,
   )
+import Data.Int (Int)
 
 instance Arbitrary Muscle where
   arbitrary = arbitraryBoundedEnum
-
-clamp :: Ord a => a -> a -> a -> a
-clamp minV maxV v = min maxV (max minV v)
-
-lengthSame :: String -> Fun (Char, NE.NonEmpty Char) Int -> Bool
-lengthSame as f =
-  length
-    ( generateComplements
-        as
-        (\x xs -> clamp 0 (NE.length xs - 1) (applyFun2 f x xs))
-    )
-    == length as
-
-elementsSame :: String -> Fun (Char, NE.NonEmpty Char) Int -> Bool
-elementsSame as f =
-  sort
-    ( generateComplements
-        as
-        (\x xs -> clamp 0 (NE.length xs - 1) (applyFun2 f x xs))
-    )
-    == sort as
 
 defaultExercise name_ muscles_ last_ =
   Exercise
@@ -114,27 +73,10 @@ instance Arbitrary Exercise where
 
 spec :: Spec
 spec = do
-  describe "generate complements" $ do
-    prop "should never change the number of list elements" lengthSame
-    prop "should only shuffle elements, not delete" elementsSame
-    prop
-      "taking the first element is the identity"
-      (\xs -> generateComplements (xs :: [Int]) (\_ _ -> 0) == xs)
-
-  describe "exercise complement" $ do
-    it "should favor small intersections" $ do
-      let referenceExercise =
-            RankedExercise (Exercise {name = "1", muscles = [Pecs, GluteusMaximus]}) 3
-          restExercises =
-            [ RankedExercise (Exercise {name = "2", muscles = [Hamstrings]}) 3,
-              RankedExercise (Exercise {name = "3", muscles = [GluteusMaximus, Hamstrings]}) 3
-            ]
-      exerciseComplement referenceExercise restExercises `shouldBe` 0
-
   describe "rank time" $ do
     it "should favor older exercises" $ do
       let oldDate = UTCTime (fromGregorian 2018 12 3) 0
-          newDate = realToFrac 60 `addUTCTime` oldDate
+          newDate = realToFrac (60 :: Int) `addUTCTime` oldDate
           oldExercise = defaultExercise "older" [Pecs] (Just oldDate)
           newExercise = defaultExercise "newer" [Pecs] (Just newDate)
       rankTime [newExercise, oldExercise] `shouldBe` [0, 100]
@@ -152,8 +94,8 @@ spec = do
 
     it "should lerp across time" $ do
       let oldDate = UTCTime (fromGregorian 2018 12 3) 0
-          middleDate = realToFrac 60 `addUTCTime` oldDate
-          newDate = realToFrac 120 `addUTCTime` oldDate
+          middleDate = realToFrac (60 :: Int) `addUTCTime` oldDate
+          newDate = realToFrac (120 :: Int) `addUTCTime` oldDate
           oldExercise = defaultExercise "older" [Pecs] (Just oldDate)
           middleExercise = defaultExercise "older" [Pecs] (Just middleDate)
           newExercise = defaultExercise "newer" [Pecs] (Just newDate)
@@ -163,7 +105,7 @@ spec = do
   describe "reorder" $ do
     it "alternate between groups" $ do
       let oldDate = UTCTime (fromGregorian 2018 12 3) 0
-          newDate = realToFrac 60 `addUTCTime` oldDate
+          newDate = realToFrac (60 :: Int) `addUTCTime` oldDate
           aEx = defaultExercise "older" [Pecs] (Just oldDate)
           bEx = defaultExercise "newer" [GluteusMaximus] (Just newDate)
           aEx2 = defaultExercise "older" [Pecs] (Just newDate)
